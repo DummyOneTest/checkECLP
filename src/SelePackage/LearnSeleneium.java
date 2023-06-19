81321234f36fc64f09df32423c4cbfdde78133fa
Anuranjan Kumar <anu.prg09@gmail.com>
2023-06-19 23:09:18 +0530

    commited by Eclipse

---
+++ src/SelePackage/LearnSeleneium.java
@@ -0,0 +1,14 @@
+package SelePackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LearnSeleneium {
+
+	public static void main(String[] args) {
+		WebDriver chrome = new ChromeDriver();
+		chrome.navigate().to("https://www.gmail.com");
+
+	}
+
+}
